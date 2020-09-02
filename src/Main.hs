f80e83036c93b448d07487f5a6255f20a5bd7746
Anton-Latukha <anton.latukha@gmail.com>
2020-09-02 18:54:03 +0300

    M  src/Main.hs: co Optparse version key

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,36 @@
 module Main where
 
+
+{- -- Optparse version
+
+import Options.Applicative
+import Data.Semigroup ((<>))
 import Data.Version (showVersion)
 import qualified Paths_zen as Package
 
+data Command
+  = Command
+  {
+    version :: Bool
+  }
+
+baseOpt :: Parser Command
+
+versionOpt :: Parser (a -> a)
+versionOpt = infoOption (showVersion Package.version) (long "version" <> help "Released version")
+
+opts :: Parser Command
+opts = baseOpt <**> helper <**> versionOpt
+
 main :: IO ()
-main = putStrLn (showVersion Package.version)
+main = greet =<< execParser opts
+  where
+    opts = info (greet <**> helper <**> versionOpt) mempty
+    greet :: Command -> IO ()
+    greet (Command False) = putStrLn $ "Hello!"
+    greet _ = return ()
+
+-}
+
+main :: IO ()
+main = putStrLn "a"
