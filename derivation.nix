52afd369a8a791765ed3b0d019d2242842d2662b
Anton-Latukha <anton.latukha@gmail.com>
2020-09-02 10:37:44 +0300

    M  derivation.nix

--- derivation.nix
+++ derivation.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, optparse-applicative, stdenv }:
 mkDerivation {
   pname = "zen";
   version = "0.0.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base optparse-applicative ];
   homepage = "https://blog.latukha.com";
   description = "Synopsis text";
   license = stdenv.lib.licenses.gpl3;
