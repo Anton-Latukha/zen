abfec9dd89b19c22839fccff8fc6ef851e84e0c8
Anton-Latukha <anton.latukha@gmail.com>
2020-09-04 10:10:20 +0300

    M  src/Main.hs: add Optparse scaffolding

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+import Options.Applicative
+
+data Opts
+  = Opts
+  {
+    optLogFile :: String
+  }
 
 {- -- Optparse version
 
@@ -36,6 +43,30 @@ logFile = "/tmp/tmp.log"
 
 main :: IO ()
 main = do
+  opts <- execParser optsParser
   text <- getContents
-  putStrLn text
-  appendFile logFile text
+  print text
+  appendFile (optLogFile opts) text
+
+ where
+  optsParser :: ParserInfo Opts
+  optsParser =
+    info
+      (helper <*> versionOption <*> programOptions)
+      (fullDesc <> progDesc "Zen. Attend to what is important." <>
+        header "zen - silence/redirect stdout outputs to get only important information.")
+
+  versionOption :: Parser (a -> a)
+  versionOption = infoOption "0.0" (long "version" <> help "Show version")
+
+  logFileOpt :: Parser String
+  logFileOpt = strOption $
+    long "file"
+    <> short 'f'
+    <> metavar "LOGFILE"
+    <> help "Direct log into a file"
+
+  programOptions :: Parser Opts
+  programOptions =
+    Opts
+    <$> logFileOpt
