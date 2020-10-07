fc564b29176550e164ae36f64299e66aa9e97063
Anton-Latukha <anton.latukha@gmail.com>
2020-10-08 01:30:48 +0300

    M  derivation.nix

--- derivation.nix
+++ derivation.nix
@@ -1,14 +1,19 @@
-{ mkDerivation, base, hsyslog, optparse-applicative, stdenv, unix
+{ mkDerivation, base, errors, hsyslog, optparse-applicative
+, process, stdenv, unix
 }:
 mkDerivation {
   pname = "zen";
   version = "0.0.0.1";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
-  executableHaskellDepends = [
-    base hsyslog optparse-applicative unix
+  libraryHaskellDepends = [
+    base errors hsyslog optparse-applicative process unix
   ];
+  executableHaskellDepends = [
+    base errors hsyslog optparse-applicative process unix
+  ];
+  doHaddock = false;
   homepage = "https://blog.latukha.com";
   description = "Synopsis text";
   license = stdenv.lib.licenses.gpl3;
